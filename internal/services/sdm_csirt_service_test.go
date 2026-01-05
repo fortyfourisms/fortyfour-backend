6b5eea7a2c52be23ff59f169b2a16225250291d6
Krisna Choiril Andika <99785580+khrisnandika@users.noreply.github.com>
2026-01-05 11:27:45 +0700

    update: sdm csirt unit test

--- internal/services/sdm_csirt_service_test.go
+++ internal/services/sdm_csirt_service_test.go
@@ -1,6 +1,11 @@
 package services
 
-import "fortyfour-backend/internal/dto"
+import (
+	"fortyfour-backend/internal/dto"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 type mockSdmCsirtRepo struct {
 	CreateFn  func(req dto.CreateSdmCsirtRequest, id string) error
@@ -24,4 +29,78 @@ func (m *mockSdmCsirtRepo) Update(id string, req dto.SdmCsirtResponse) error {
 }
 func (m *mockSdmCsirtRepo) Delete(id string) error {
 	return m.DeleteFn(id)
+}
+
+func strPtr(s string) *string {
+	return &s
+}
+
+func TestSdmCsirtService_Create(t *testing.T) {
+	repo := &mockSdmCsirtRepo{
+		CreateFn: func(req dto.CreateSdmCsirtRequest, id string) error {
+			return nil
+		},
+	}
+
+	service := NewSdmCsirtService(repo)
+
+	id, err := service.Create(dto.CreateSdmCsirtRequest{
+		NamaPersonel: strPtr("Andi"),
+	})
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, id)
+}
+
+func TestSdmCsirtService_GetAll(t *testing.T) {
+	repo := &mockSdmCsirtRepo{
+		GetAllFn: func() ([]dto.SdmCsirtResponse, error) {
+			return []dto.SdmCsirtResponse{
+				{NamaPersonel: "Budi"},
+			}, nil
+		},
+	}
+
+	service := NewSdmCsirtService(repo)
+
+	res, err := service.GetAll()
+
+	assert.NoError(t, err)
+	assert.Len(t, res, 1)
+}
+
+func TestSdmCsirtService_Update(t *testing.T) {
+	repo := &mockSdmCsirtRepo{
+		GetByIDFn: func(id string) (*dto.SdmCsirtResponse, error) {
+			return &dto.SdmCsirtResponse{
+				NamaPersonel: "Old",
+			}, nil
+		},
+		UpdateFn: func(id string, req dto.SdmCsirtResponse) error {
+			return nil
+		},
+	}
+
+	service := NewSdmCsirtService(repo)
+
+	newName := "New"
+	err := service.Update("1", dto.UpdateSdmCsirtRequest{
+		NamaPersonel: &newName,
+	})
+
+	assert.NoError(t, err)
+}
+
+func TestSdmCsirtService_Delete(t *testing.T) {
+	repo := &mockSdmCsirtRepo{
+		DeleteFn: func(id string) error {
+			return nil
+		},
+	}
+
+	service := NewSdmCsirtService(repo)
+
+	err := service.Delete("1")
+
+	assert.NoError(t, err)
 }
