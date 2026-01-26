fe158632d653302c99559905a106a42eb0d140a1
Maula Afiif <maulanajmipn@gmail.com>
2026-01-26 09:08:24 +0700

    chore: update sub_sektor names and add new subsektor

--- migrations/009_seed_sub_sektor_table.sql
+++ migrations/009_seed_sub_sektor_table.sql
@@ -1,48 +1,52 @@
 -- ILMATE
 INSERT INTO sub_sektor (id, id_sektor, nama_sub_sektor, created_at, updated_at)
-SELECT UUID(), s.id, 'Elektronik', NOW(), NOW()
+SELECT UUID(), s.id, 'Logam', NOW(), NOW()
 FROM sektor s WHERE s.nama_sektor = 'ILMATE';
 
 INSERT INTO sub_sektor (id, id_sektor, nama_sub_sektor, created_at, updated_at)
-SELECT UUID(), s.id, 'Otomotif', NOW(), NOW()
+SELECT UUID(), s.id, 'Permesinan & alat pertanian', NOW(), NOW()
 FROM sektor s WHERE s.nama_sektor = 'ILMATE';
 
 INSERT INTO sub_sektor (id, id_sektor, nama_sub_sektor, created_at, updated_at)
-SELECT UUID(), s.id, 'Keamanan Siber', NOW(), NOW()
+SELECT UUID(), s.id, 'Transportasi, maritim & pertahanan', NOW(), NOW()
+FROM sektor s WHERE s.nama_sektor = 'ILMATE';
+
+INSERT INTO sub_sektor (id, id_sektor, nama_sub_sektor, created_at, updated_at)
+SELECT UUID(), s.id, 'Elektronika & telematika', NOW(), NOW()
 FROM sektor s WHERE s.nama_sektor = 'ILMATE';
 
 
--- Industri Agro
+-- Agro
 INSERT INTO sub_sektor (id, id_sektor, nama_sub_sektor, created_at, updated_at)
-SELECT UUID(), s.id, 'Agro Bisnis', NOW(), NOW()
-FROM sektor s WHERE s.nama_sektor = 'Industri Agro';
+SELECT UUID(), s.id, 'Hasil hutan & perkebunan', NOW(), NOW()
+FROM sektor s WHERE s.nama_sektor = 'Agro';
 
 INSERT INTO sub_sektor (id, id_sektor, nama_sub_sektor, created_at, updated_at)
-SELECT UUID(), s.id, 'Konstruksi', NOW(), NOW()
-FROM sektor s WHERE s.nama_sektor = 'Industri Agro';
+SELECT UUID(), s.id, 'Pangan & perikanan', NOW(), NOW()
+FROM sektor s WHERE s.nama_sektor = 'Agro';
 
 INSERT INTO sub_sektor (id, id_sektor, nama_sub_sektor, created_at, updated_at)
-SELECT UUID(), s.id, 'Jasa', NOW(), NOW()
-FROM sektor s WHERE s.nama_sektor = 'Industri Agro';
+SELECT UUID(), s.id, 'Minuman, tembakau & bahan penyegar', NOW(), NOW()
+FROM sektor s WHERE s.nama_sektor = 'Agro';
 
 INSERT INTO sub_sektor (id, id_sektor, nama_sub_sektor, created_at, updated_at)
-SELECT UUID(), s.id, 'Surveyor', NOW(), NOW()
-FROM sektor s WHERE s.nama_sektor = 'Industri Agro';
+SELECT UUID(), s.id, 'Kemurgi, oleokimia & pakan', NOW(), NOW()
+FROM sektor s WHERE s.nama_sektor = 'Agro';
 
 
 -- IKFT
 INSERT INTO sub_sektor (id, id_sektor, nama_sub_sektor, created_at, updated_at)
-SELECT UUID(), s.id, 'Tekstil', NOW(), NOW()
+SELECT UUID(), s.id, 'Kimia hulu', NOW(), NOW()
 FROM sektor s WHERE s.nama_sektor = 'IKFT';
 
 INSERT INTO sub_sektor (id, id_sektor, nama_sub_sektor, created_at, updated_at)
-SELECT UUID(), s.id, 'Kimia', NOW(), NOW()
+SELECT UUID(), s.id, 'Kimia hilir & farmasi', NOW(), NOW()
 FROM sektor s WHERE s.nama_sektor = 'IKFT';
 
 INSERT INTO sub_sektor (id, id_sektor, nama_sub_sektor, created_at, updated_at)
-SELECT UUID(), s.id, 'Kawasan Industri', NOW(), NOW()
+SELECT UUID(), s.id, 'Semen, keramik & nonlogam', NOW(), NOW()
 FROM sektor s WHERE s.nama_sektor = 'IKFT';
 
 INSERT INTO sub_sektor (id, id_sektor, nama_sub_sektor, created_at, updated_at)
-SELECT UUID(), s.id, 'Farmasi', NOW(), NOW()
+SELECT UUID(), s.id, 'Tekstil, kulit & alas kaki', NOW(), NOW()
 FROM sektor s WHERE s.nama_sektor = 'IKFT';
