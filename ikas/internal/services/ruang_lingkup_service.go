df25d4817f78df418f1423fb80cd4092a734fe33
Krisna Choiril Andika <99785580+khrisnandika@users.noreply.github.com>
2026-02-05 09:58:55 +0700

    refactor: move input validation to utils

--- ikas/internal/services/ruang_lingkup_service.go
+++ ikas/internal/services/ruang_lingkup_service.go
@@ -5,8 +5,7 @@ import (
 	"errors"
 	"ikas/internal/dto"
 	"ikas/internal/repository"
-	"regexp"
-	"strings"
+	"ikas/internal/utils"
 
 	"github.com/google/uuid"
 	"github.com/rollbar/rollbar-go"
@@ -20,73 +19,10 @@ func NewRuangLingkupService(repo repository.RuangLingkupRepositoryInterface) *Ru
 	return &RuangLingkupService{repo: repo}
 }
 
-// Normalisasi input: trim dan hilangkan multiple spaces
-func normalizeInput(input string) string {
-	input = strings.TrimSpace(input)
-
-	multipleSpaces := regexp.MustCompile(`\s+`)
-	input = multipleSpaces.ReplaceAllString(input, " ")
-
-	return input
-}
-
-// Validasi karakter berbahaya untuk SQL Injection
-func containsSQLInjectionPattern(input string) bool {
-	// Pattern umum SQL injection
-	dangerousPatterns := []string{
-		"--",         // SQL comment
-		";",          // Multiple statements
-		"'",          // String delimiter
-		"\"",         // String delimiter
-		"/*",         // Multi-line comment
-		"*/",         // Multi-line comment
-		"xp_",        // Extended stored procedures
-		"sp_",        // Stored procedures
-		"exec",       // Execute command
-		"execute",    // Execute command
-		"drop",       // Drop command
-		"insert",     // Insert command
-		"delete",     // Delete command
-		"update",     // Update command
-		"union",      // Union query
-		"select",     // Select command
-		"create",     // Create command
-		"alter",      // Alter command
-		"shutdown",   // Shutdown command
-		"script",     // Script tag
-		"javascript", // JavaScript
-		"<script",    // Script tag
-		"</script>",  // Script tag
-		"onerror",    // Event handler
-		"onload",     // Event handler
-	}
-
-	lowerInput := strings.ToLower(input)
-	for _, pattern := range dangerousPatterns {
-		if strings.Contains(lowerInput, pattern) {
-			return true
-		}
-	}
-	return false
-}
-
-// Validasi karakter yang diizinkan
-func isValidInput(input string) bool {
-	// Hanya izinkan: huruf, angka, spasi, dan beberapa karakter khusus umum
-	validPattern := regexp.MustCompile(`^[a-zA-Z0-9\s\-_.,()&]+$`)
-	return validPattern.MatchString(input)
-}
-
-// Validasi UUID format untuk mencegah injection via ID
-func isValidUUID(id string) bool {
-	uuidPattern := regexp.MustCompile(`^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$`)
-	return uuidPattern.MatchString(id)
-}
-
 // Validasi untuk Create
 func (s *RuangLingkupService) validateCreate(req *dto.CreateRuangLingkupRequest) error {
 	// Normalisasi: trim whitespace + hilangkan multiple spaces
-	req.NamaRuangLingkup = normalizeInput(req.NamaRuangLingkup)
+	req.NamaRuangLingkup = utils.NormalizeInput(req.NamaRuangLingkup)
 
 	// NOT NULL: tidak boleh kosong
 	if req.NamaRuangLingkup == "" {
@@ -104,12 +40,12 @@ func (s *RuangLingkupService) validateCreate(req *dto.CreateRuangLingkupRequest)
 	}
 
 	// Validasi SQL Injection pattern (blacklist)
-	if containsSQLInjectionPattern(req.NamaRuangLingkup) {
+	if utils.ContainsSQLInjectionPattern(req.NamaRuangLingkup) {
 		return errors.New("nama_ruang_lingkup mengandung karakter yang tidak diizinkan")
 	}
 
 	// Validasi karakter yang diizinkan
-	if !isValidInput(req.NamaRuangLingkup) {
+	if !utils.IsValidInput(req.NamaRuangLingkup) {
 		return errors.New("nama_ruang_lingkup hanya boleh mengandung huruf, angka, spasi, dan karakter -_.,()&")
 	}
 
@@ -121,7 +57,7 @@ func (s *RuangLingkupService) validateUpdate(req *dto.UpdateRuangLingkupRequest)
 	// Jika field dikirim (bukan nil), lakukan validasi
 	if req.NamaRuangLingkup != nil {
 		// Normalisasi: trim whitespace + hilangkan multiple spaces
-		normalized := normalizeInput(*req.NamaRuangLingkup)
+		normalized := utils.NormalizeInput(*req.NamaRuangLingkup)
 		req.NamaRuangLingkup = &normalized
 
 		// NOT NULL: tidak boleh string kosong
@@ -140,12 +76,12 @@ func (s *RuangLingkupService) validateUpdate(req *dto.UpdateRuangLingkupRequest)
 		}
 
 		// Validasi SQL Injection pattern (blacklist)
-		if containsSQLInjectionPattern(*req.NamaRuangLingkup) {
+		if utils.ContainsSQLInjectionPattern(*req.NamaRuangLingkup) {
 			return errors.New("nama_ruang_lingkup mengandung karakter yang tidak diizinkan")
 		}
 
 		// Validasi karakter yang diizinkan (whitelist - lebih ketat)
-		if !isValidInput(*req.NamaRuangLingkup) {
+		if !utils.IsValidInput(*req.NamaRuangLingkup) {
 			return errors.New("nama_ruang_lingkup hanya boleh mengandung huruf, angka, spasi, dan karakter -_.,()&")
 		}
 	}
@@ -193,7 +129,7 @@ func (s *RuangLingkupService) GetAll() ([]dto.RuangLingkupResponse, error) {
 
 func (s *RuangLingkupService) GetByID(id string) (*dto.RuangLingkupResponse, error) {
 	// Validasi format UUID untuk mencegah SQL injection via ID
-	if !isValidUUID(id) {
+	if !utils.IsValidUUID(id) {
 		return nil, errors.New("format ID tidak valid")
 	}
 
@@ -209,7 +145,7 @@ func (s *RuangLingkupService) GetByID(id string) (*dto.RuangLingkupResponse, err
 
 func (s *RuangLingkupService) Update(id string, req dto.UpdateRuangLingkupRequest) (*dto.RuangLingkupResponse, error) {
 	// Validasi format UUID untuk mencegah SQL injection via ID
-	if !isValidUUID(id) {
+	if !utils.IsValidUUID(id) {
 		return nil, errors.New("format ID tidak valid")
 	}
 
@@ -257,7 +193,7 @@ func (s *RuangLingkupService) Update(id string, req dto.UpdateRuangLingkupReques
 
 func (s *RuangLingkupService) Delete(id string) error {
 	// Validasi format UUID untuk mencegah SQL injection via ID
-	if !isValidUUID(id) {
+	if !utils.IsValidUUID(id) {
 		return errors.New("format ID tidak valid")
 	}
 
