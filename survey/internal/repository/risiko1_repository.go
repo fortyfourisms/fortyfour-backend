f538beeb08dd0efb6f2bd8f76a5c4b5a511f1142
Alfia Naila Salsabila <93040208+alfianaila@users.noreply.github.com>
2026-03-30 14:36:31 +0700

    risk alignment of repository with database

--- survey/internal/repository/risiko1_repository.go
+++ survey/internal/repository/risiko1_repository.go
@@ -2,23 +2,6 @@ package repository
 
 import "database/sql"
 
-type Risiko struct {
-	ID                    int
-	NamaRisiko            string
-	Deskripsi             string
-
-	PotensiKejadian       string
-	DampakReputasi        string
-	DampakOperasional     string
-	DampakFinansial       string
-	DampakHukum           string
-
-	Frekuensi             string
-
-	AdaPengendalian       string
-	DeskripsiPengendalian string
-}
-
 type RisikoRepository struct {
 	db *sql.DB
 }
@@ -27,75 +10,37 @@ func NewRisikoRepository(db *sql.DB) *RisikoRepository {
 	return &RisikoRepository{db: db}
 }
 
-// CREATE
-func (r *RisikoRepository) Create(data Risiko) (Risiko, error) {
-
-	query := `
-	INSERT INTO risiko 
-	(nama_risiko, deskripsi, potensi_kejadian, dampak_reputasi,
-	dampak_operasional, dampak_finansial, dampak_hukum,
-	frekuensi, ada_pengendalian, deskripsi_pengendalian)
-	VALUES (?,?,?,?,?,?,?,?,?,?)
-	`
-
-	res, err := r.db.Exec(query,
-		data.NamaRisiko,
-		data.Deskripsi,
-		data.PotensiKejadian,
-		data.DampakReputasi,
-		data.DampakOperasional,
-		data.DampakFinansial,
-		data.DampakHukum,
-		data.Frekuensi,
-		data.AdaPengendalian,
-		data.DeskripsiPengendalian,
-	)
-
-	if err != nil {
-		return data, err
-	}
-
-	id, _ := res.LastInsertId()
-	data.ID = int(id)
-
-	return data, nil
+// MASTER RISIKO
+func (r *RisikoRepository) GetAllRisiko() (*sql.Rows, error) {
+	return r.db.Query(`SELECT id, nama_risiko, deskripsi FROM risiko`)
 }
 
-// GET ALL
-func (r *RisikoRepository) GetAll() ([]Risiko, error) {
+// JAWABAN RISIKO
+func (r *RisikoRepository) CreateJawaban(
+	req map[string]interface{},
+) error {
 
-	rows, err := r.db.Query(`SELECT * FROM risiko`)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+	query := `
+	INSERT INTO risiko_responden
+	(responden_id, risiko_id, pernah_terjadi,
+	dampak_reputasi, dampak_operasional, dampak_finansial, dampak_hukum,
+	frekuensi, ada_pengendalian, deskripsi_pengendalian)
+	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+	`
 
-	var list []Risiko
+	_, err := r.db.Exec(
+		query,
+		req["responden_id"],
+		req["risiko_id"],
+		req["pernah_terjadi"],
+		req["dampak_reputasi"],
+		req["dampak_operasional"],
+		req["dampak_finansial"],
+		req["dampak_hukum"],
+		req["frekuensi"],
+		req["ada_pengendalian"],
+		req["deskripsi_pengendalian"],
+	)
 
-	for rows.Next() {
-		var d Risiko
-
-		err := rows.Scan(
-			&d.ID,
-			&d.NamaRisiko,
-			&d.Deskripsi,
-			&d.PotensiKejadian,
-			&d.DampakReputasi,
-			&d.DampakOperasional,
-			&d.DampakFinansial,
-			&d.DampakHukum,
-			&d.Frekuensi,
-			&d.AdaPengendalian,
-			&d.DeskripsiPengendalian,
-			new(interface{}),
-		)
-
-		if err != nil {
-			return nil, err
-		}
-
-		list = append(list, d)
-	}
-
-	return list, nil
+	return err
 }
