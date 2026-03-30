b0a6b2a1ddd87ce149fdcc518aeae8c9554b530a
Alfia Naila Salsabila <93040208+alfianaila@users.noreply.github.com>
2026-03-30 13:22:51 +0700

    update risk of Theft of Company Intellectual Property in services folder

--- survey/internal/services/risiko1_service.go
+++ survey/internal/services/risiko1_service.go
@@ -1,8 +1,8 @@
 package services
 
 import (
+	"errors"
 	"survey/internal/dto"
-	"survey/internal/models"
 	"survey/internal/repository"
 )
 
@@ -11,22 +11,34 @@ type RisikoService struct {
 }
 
 func NewRisikoService(r *repository.RisikoRepository) *RisikoService {
-	return &RisikoService{repo: r}
+	return &RisikoService{r}
 }
 
-func (s *RisikoService) Create(req dto.CreateRisikoRequest) error {
+func (s *RisikoService) Create(req dto.CreateRisikoRequest) (dto.RisikoResponse, error) {
 
-	data := models.RisikoSurvey{
-		RespondenID: req.RespondenID,
-		RisikoIP: req.RisikoIP,
-		DampakReputasi: req.DampakReputasi,
-		DampakOperasional: req.DampakOperasional,
-		DampakFinansial: req.DampakFinansial,
-		DampakHukum: req.DampakHukum,
-		Frekuensi: req.Frekuensi,
-		AdaPengendalian: req.AdaPengendalian,
-		TindakanPengendalian: req.TindakanPengendalian,
+	if req.NamaRisiko == "" {
+		return dto.RisikoResponse{}, errors.New("nama risiko wajib diisi")
 	}
 
-	return s.repo.Create(data)
+	data, err := s.repo.Create(repository.Risiko(req))
+	if err != nil {
+		return dto.RisikoResponse{}, err
+	}
+
+	return dto.RisikoResponse(data), nil
+}
+
+func (s *RisikoService) GetAll() ([]dto.RisikoResponse, error) {
+
+	list, err := s.repo.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var res []dto.RisikoResponse
+	for _, v := range list {
+		res = append(res, dto.RisikoResponse(v))
+	}
+
+	return res, nil
 }
