17632079a83cfe1c55f1457457da55d316e2a1c6
Maula Afiif <maulanajmipn@gmail.com>
2026-04-09 10:43:13 +0700

    feat: add email field to pic perusahaan

--- internal/dto/pic_perusahaan_dto.go
+++ internal/dto/pic_perusahaan_dto.go
@@ -3,12 +3,14 @@ package dto
 type CreatePICRequest struct {
 	Nama         *string `json:"nama,omitempty"`
 	Telepon      *string `json:"telepon,omitempty"`
+	Email        *string `json:"email,omitempty"`
 	IDPerusahaan *string `json:"id_perusahaan,omitempty"`
 }
 
 type UpdatePICRequest struct {
 	Nama         *string `json:"nama,omitempty"`
 	Telepon      *string `json:"telepon,omitempty"`
+	Email        *string `json:"email,omitempty"`
 	IDPerusahaan *string `json:"id_perusahaan,omitempty"`
 }
 
@@ -16,6 +18,7 @@ type PICResponse struct {
 	ID         string           `json:"id"`
 	Nama       string           `json:"nama"`
 	Telepon    string           `json:"telepon"`
+	Email      string           `json:"email"`
 	CreatedAt  string           `json:"created_at"`
 	UpdatedAt  string           `json:"updated_at"`
 	Perusahaan *PerusahaanInPIC `json:"perusahaan,omitempty"`
