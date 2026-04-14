b634d3827dac974d01dddd994d3b94d838bdd2f3
Krisna Choiril Andika <99785580+khrisnandika@users.noreply.github.com>
2026-04-14 09:52:23 +0700

    update: IKAS migrations

--- migrations/074_add_created_updated_to_pertanyaan_and_jawaban.down.sql
+++ migrations/074_add_created_updated_to_pertanyaan_and_jawaban.down.sql
@@ -13,3 +13,19 @@ DROP COLUMN IF EXISTS updated_at;
 ALTER TABLE pertanyaan_gulih 
 DROP COLUMN IF EXISTS created_at,
 DROP COLUMN IF EXISTS updated_at;
+
+ALTER TABLE jawaban_identifikasi 
+DROP COLUMN IF EXISTS created_at,
+DROP COLUMN IF EXISTS updated_at;
+
+ALTER TABLE jawaban_proteksi 
+DROP COLUMN IF EXISTS created_at,
+DROP COLUMN IF EXISTS updated_at;
+
+ALTER TABLE jawaban_deteksi 
+DROP COLUMN IF EXISTS created_at,
+DROP COLUMN IF EXISTS updated_at;
+
+ALTER TABLE jawaban_gulih 
+DROP COLUMN IF EXISTS created_at,
+DROP COLUMN IF EXISTS updated_at;
