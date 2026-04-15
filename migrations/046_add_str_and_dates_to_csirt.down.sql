fc6d9e59023a6e79be20200da5aeb93266d1b7ae
Ryan Martin <ryan.martin3003@gmail.com>
2026-04-15 08:42:36 +0700

    featured: fix sequence migration

--- migrations/046_add_str_and_dates_to_csirt.down.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE csirt
-    DROP COLUMN file_str,
-    DROP COLUMN tanggal_registrasi,
-    DROP COLUMN tanggal_kadaluarsa,
-    DROP COLUMN tanggal_registrasi_ulang;
--- migrations/046_add_str_and_dates_to_csirt.up.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE csirt
-    ADD COLUMN file_str          VARCHAR(255) NULL,
-    ADD COLUMN tanggal_registrasi     DATE     NULL,
-    ADD COLUMN tanggal_kadaluarsa     DATE     NULL,
-    ADD COLUMN tanggal_registrasi_ulang DATE   NULL;
